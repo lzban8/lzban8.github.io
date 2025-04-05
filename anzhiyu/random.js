2c65f1c23eaa45838175f0ec4b31d2d06445c8ee
lzban8 <lzban8@users.noreply.github.com>
2025-04-05 21:05:08 +0800

    Site updated: 2025-04-05 21:05:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/05/hello-world/","2025/04/05/Xboard前端-1Panel部署/","2025/04/05/XrayR 后端脚本/"];function toRandomPost(){
+var posts=["2025/04/05/hello-world/","2025/04/05/XrayR 后端脚本/","2025/04/05/Xboard前端-1Panel部署/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
